9de694f6fcff3b11efafd8a99e140813723d2e4f
Ishika Mogha <moghaishika08@gmail.com>
2025-07-12 15:23:27 +0530

    Update indexx.js

--- indexx.js
+++ indexx.js
@@ -19,25 +19,25 @@ document.addEventListener("keydown", function (event) {
 function makeSound(key) {
   switch (key) {
     case "w":
-      new Audio("sounds/tom-1.mp3").play();
+      new Audio("tom-1.mp3").play();
       break;
     case "a":
-      new Audio("sounds/tom-2.mp3").play();
+      new Audio("tom-2.mp3").play();
       break;
     case "s":
-      new Audio("sounds/tom-3.mp3").play();
+      new Audio("tom-3.mp3").play();
       break;
     case "d":
-      new Audio("sounds/tom-4.mp3").play();
+      new Audio("tom-4.mp3").play();
       break;
     case "j":
-      new Audio("sounds/snare.mp3").play();
+      new Audio("snare.mp3").play();
       break;
     case "k":
-      new Audio("sounds/crash.mp3").play();
+      new Audio("crash.mp3").play();
       break;
     case "l":
-      new Audio("sounds/kick-bass.mp3").play();
+      new Audio("kick-bass.mp3").play();
       break;
     default:
       console.log(key + " is not a valid drum key.");
